81ec4b66ae5dc4f2c70daf364efc95d0f98819f8
Pasha <pavik.varganov@gmail.com>
2024-10-22 15:29:38 +0300

    remove redundant settings

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -186,33 +186,3 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173"
 ]
-
-# CORS_ALLOW_ALL_ORIGINS = True
-#
-# CORS_ALLOW_METHODS = [
-#     "GET",
-#     "POST",
-#     "PUT",
-#     "PATCH",
-#     "DELETE",
-#     "OPTIONS",
-# ]
-#
-# CORS_ALLOW_HEADERS = [
-#     "accept",
-#     "accept-encoding",
-#     "authorization",
-#     "content-type",
-#     "dnt",
-#     "origin",
-#     "user-agent",
-#     "x-csrftoken",
-#     "x-requested-with",
-# ]
-#
-# CORS_ALLOW_CREDENTIALS = True
-
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173"
-# ]
